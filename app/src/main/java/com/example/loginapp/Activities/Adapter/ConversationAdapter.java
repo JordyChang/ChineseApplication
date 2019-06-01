9709b4246e5e3ce9b523fe3673cd95f9efd2d443
JordyChang <JordyChang@users.noreply.github.com>
2019-06-01 14:43:19 +0800

    Revert "Kontol"
    This reverts commit d2a633cced2c916ae4ca9b26ebac5ac1863278c3.

--- app/src/main/java/com/example/loginapp/Activities/Adapter/ConversationAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.loginapp.Activities.Adapter;
-
-public class ConversationAdapter {
-}
--- app/src/main/java/com/example/loginapp/Activities/Model/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.loginapp.Activities.Model;
-
-public class User {
-    public String username;
-    public String email;
-    public String user_id;
-
-
-}
